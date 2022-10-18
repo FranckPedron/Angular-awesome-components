48de01fcafdfc629e92bd1c210816a27ac2ad4fb
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-10-18 17:44:29 +0200

    Build - add comment function

--- src/app/social-media/components/post-list/post-list.component.ts
+++ src/app/social-media/components/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from '../../models/post.model';
 import {map, Observable} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
+import {PostsService} from "../../services/posts.service";
 
 @Component({
   selector: 'app-post-list',
@@ -12,7 +13,8 @@ export class PostListComponent implements OnInit {
 
   posts$!: Observable<Post[]>;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private postsService: PostsService) { }
 
   ngOnInit(): void {
     this.posts$ = this.route.data.pipe(
@@ -20,4 +22,7 @@ export class PostListComponent implements OnInit {
     );
   }
 
+  onPosCommented(postCommented: {comment: string, postId: number}) {
+    this.postsService.addNewComment(postCommented);
+  }
 }
