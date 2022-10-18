1d314579429454efb24fc51aaf02e97ec7df61df
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-10-18 17:56:48 +0200

    Build - add Pipe shorten

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import {CommonModule} from '@angular/common';
 import {CommentsComponent} from './components/comments/comments.component';
 import {MaterialModule} from "./material.module";
 import {ReactiveFormsModule} from "@angular/forms";
+import {ShortenPipe} from "./pipes/shorten.pipe";
 
 @NgModule({
   declarations: [
-    CommentsComponent
+    CommentsComponent,
+    ShortenPipe
   ],
   imports: [
     CommonModule,
@@ -16,7 +18,8 @@ import {ReactiveFormsModule} from "@angular/forms";
   exports: [
     CommentsComponent,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ShortenPipe
   ]
 })
 export class SharedModule {
