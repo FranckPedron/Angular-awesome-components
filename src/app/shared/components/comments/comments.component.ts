a3023d8aff3225f0bd1c756bd0bd29cbd7c24a75
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-10-19 11:22:26 +0200

    Build - add animation on comments

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,11 +1,32 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormControl, Validators} from '@angular/forms';
 import {Comment} from "../../../core/models/comment.model";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        'background-color': 'white',
+        'z-index': 1
+      })),
+      state('active', style({
+        transform: 'scale(1.05)',
+        'background-color': 'rgb(201,157,242)',
+        'z-index': 2
+      })),
+      transition('default => active', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit {
 
@@ -13,6 +34,7 @@ export class CommentsComponent implements OnInit {
   @Output() newComment = new EventEmitter<string>();
 
   commentCtrl!: FormControl;
+  listItemAnimationState: 'default' | 'active' = 'default';
 
   constructor(private formBuilder: FormBuilder) {
   }
@@ -27,5 +49,13 @@ export class CommentsComponent implements OnInit {
     }
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
+  }
+
+  onListItemMouseEnter() {
+    this.listItemAnimationState = 'active';
+  }
+
+  onListItemMouseLeave() {
+    this.listItemAnimationState = 'default';
   }
 }
--- src/app/social-media/components/post-list/post-list.component.ts
+++ src/app/social-media/components/post-list/post-list.component.ts
@@ -7,7 +7,8 @@ import {PostsService} from "../../services/posts.service";
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+  styleUrls: ['./post-list.component.scss'],
+
 })
 export class PostListComponent implements OnInit {
 
