c4c7a6f62481e8ba5835141a263fce7fa4116a7d
FranckPedron <franckyp1+rapports_git@gmail.com>
2023-01-17 11:11:44 +0100

    Build - add candidates loading

--- src/app/reactive-state/services/candidates.service.ts
+++ src/app/reactive-state/services/candidates.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@angular/core";
-import {BehaviorSubject, Observable} from "rxjs";
+import {BehaviorSubject, delay, Observable, tap} from "rxjs";
 import {Candidate} from "../models/candidate.model";
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Injectable()
 export class CandidatesService {
@@ -15,11 +16,22 @@ export class CandidatesService {
   }
 
   private _candidates$ = new BehaviorSubject<Candidate[]>([]);
-  get candidates(): Observable<Candidate[]> {
+  get candidates$(): Observable<Candidate[]> {
     return this._candidates$.asObservable();
   }
 
   private setLoadingStatus(loading: boolean) {
     this._loading$.next(loading);
+  }
+
+  getCandidatesFromServer() {
+    this.setLoadingStatus(true);
+    this.http.get<Candidate[]>(`${environment.apiUrl}/candidates`).pipe(
+      delay(2000),
+      tap(candidates => {
+        this._candidates$.next(candidates);
+        this.setLoadingStatus(false);
+      })
+    ).subscribe();
   }
 }
